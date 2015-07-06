ab656d9256a9dd88d5b3155fe1aee2db397eb190
Bryan Holdt <BHoldt@paraport.com>
2015-07-06 13:17:10 -0700

    Delete ability added

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
+require 'json'
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'mongo_mapper'
+require './model/item'
 
 class TodoList < Sinatra::Base
 
@@ -14,6 +16,40 @@ class TodoList < Sinatra::Base
     @items = Item.all(:order => :created.desc)
     redirect '/new' if @items.empty?
     erb :index
+  end
+
+  get "/new" do
+    @title = "Add todo Item"
+    erb :new
+  end
+
+  post "/new" do
+    Item.create!(:content => params[:content], :created => Time.now)
+    redirect '/'
+  end
+
+  post "/done" do
+    item = Item.first(:id => params[:id])
+    item.done = !item.done
+    item.save
+    content_type 'application/json'
+    value = item.done ? 'done' : 'not done'
+    {:id => params[:id], :status => value}.to_json
+  end
+
+  get "/delete/:id" do
+    @item = Item.first(:id => params[:id])
+    erb :delete
+  end
+
+  post "/delete/:id" do
+    if params.has_key?("ok")
+      item = Item.first(:id => params[:id])
+      item.destroy
+      redirect '/'
+    else
+      redirect '/'
+    end
   end
 
 end
