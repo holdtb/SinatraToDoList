317526b04b694a13a2ee983054b434ebf3c80da3
Bryan Holdt <BHoldt@paraport.com>
2015-07-06 12:07:54 -0700

    View now displays all todo list items

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
-require 'sinatra'
+require 'rubygems'
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'mongo_mapper'
 
-get '/' do
-  "Hello World"
+class TodoList < Sinatra::Base
+
+  configure :development do
+   MongoMapper.connection = Mongo::Connection.new("localhost")
+   MongoMapper.database = "TodoList"
+ end
+
+  get "/" do
+    @items = Item.all(:order => :created.desc)
+    redirect '/new' if @items.empty?
+    erb :index
+  end
+
 end
